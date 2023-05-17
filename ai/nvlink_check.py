7ac4b06534c5d8df3dc1c1942cf762d6a0ec9cea
whitelilis <lz@baihai.ai>
2023-05-17 14:33:28 +0800

    add nvlink check script

---
+++ ai/nvlink_check.py
@@ -0,0 +1,56 @@
+import re
+import subprocess
+import time
+import json
+
+cmd = 'nvidia-smi nvlink -gt d'.split(' ')
+
+
+all = []
+
+sleep_sec = 5
+times = 20
+
+
+for f in range(times):
+    outl = str(subprocess.check_output(cmd)).split('\n')
+    
+    k1 = ""
+    
+    ret = {"ts": time.time()}
+    
+    for line in outl:
+        line = line.strip()
+        parts = re.split('\s+', line)
+        if parts[0] == ('GPU'):
+            k1 = parts[1]
+        if parts[-1] == 'KiB':
+            k2 = parts[-3]
+            v = int(parts[-2])
+            if len(k1) > 1:
+                k = k1 + "_" + k2
+                if k in ret:
+                    ret[k] = ret[k] + v
+                else:
+                    ret[k] = v
+    
+    all.append(ret)
+    time.sleep(sleep_sec)
+
+
+last = all[-1]
+first = all[0]
+
+summary = {}
+
+ts_diff = last['ts'] - first['ts']
+
+
+for k in last.keys():
+    summary[k] = (last[k] - first[k]) / ts_diff
+
+to_write = {'raw': all, 'sum': summary}
+
+json.dump(to_write, open('/tmp/nv.json', 'w'))
+
+print(summary)
