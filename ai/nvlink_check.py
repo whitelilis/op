61a54ca0448ff3991853740a8e694e4a861189f5
whitelilis <lz@baihai.ai>
2023-05-29 16:55:11 +0800

    sync nvlink_check.py with online

--- ai/nvlink_check.py
+++ ai/nvlink_check.py
@@ -3,22 +3,34 @@ import subprocess
 import time
 import json
 
+
+def check_one(all):
+    if len(all) < 2:
+        return
+    else:
+        last = all[-1]
+        first = all[0]
+        summary = {}
+        ts_diff = last['ts'] - first['ts']
+
+        for k in last.keys():
+            summary[k] = (last[k] - first[k]) / ts_diff
+        to_write = {'raw': all, 'sum': summary}
+        json.dump(to_write, open('/tmp/nv.json', 'w'))
+        print(summary)
+
+
 cmd = 'nvidia-smi nvlink -gt d'.split(' ')
-
-
 all = []
 
 sleep_sec = 5
 times = 20
 
-
 for f in range(times):
-    outl = str(subprocess.check_output(cmd)).split('\n')
-    
+    outl = str(subprocess.check_output(cmd)).split('\\n')
     k1 = ""
-    
     ret = {"ts": time.time()}
-    
+
     for line in outl:
         line = line.strip()
         parts = re.split('\s+', line)
@@ -33,24 +45,9 @@ for f in range(times):
                     ret[k] = ret[k] + v
                 else:
                     ret[k] = v
-    
+
     all.append(ret)
+    check_one(all)
     time.sleep(sleep_sec)
 
-
-last = all[-1]
-first = all[0]
-
-summary = {}
-
-ts_diff = last['ts'] - first['ts']
-
-
-for k in last.keys():
-    summary[k] = (last[k] - first[k]) / ts_diff
-
-to_write = {'raw': all, 'sum': summary}
-
-json.dump(to_write, open('/tmp/nv.json', 'w'))
-
-print(summary)
+check_one(all)
